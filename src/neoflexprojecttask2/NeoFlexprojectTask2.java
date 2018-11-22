8f8c643b058ac049d09e30d853745a1d86f1085b
normalek <filmacker@gmail.com>
2018-11-22 23:21:22 +0300

    Comments according the project

--- src/neoflexprojecttask2/NeoFlexprojectTask2.java
+++ src/neoflexprojecttask2/NeoFlexprojectTask2.java
@@ -1,13 +1,15 @@
 package neoflexprojecttask2;
 
-import java.io.*;
-import java.text.*;
-import java.util.*;
-import java.util.logging.Level;
 import org.apache.log4j.Logger;
 
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.Iterator;
+
 public class NeoFlexprojectTask2 {
-    private static final Logger logger = Logger.getLogger(NeoFlexprojectTask2.class);
+    private static final Logger logger = Logger.getLogger(NeoFlexprojectTask2.class); // имена констант всегда с больших букв - LOGGER
 
     static Hashtable<String,Integer> countWords;
 
@@ -54,19 +56,19 @@ public class NeoFlexprojectTask2 {
         return text.split("_");
     }
     
-    public static String readFile() throws FileNotFoundException, IOException, Exception{        
-        try(InputStream f1 = new FileInputStream("input.txt");
-            BufferedInputStream bis = new BufferedInputStream(f1)) {
-            byte b[] = new byte[f1.available()];
+    public static String readFile() throws Exception{//если метод может кидать Exception, то остальные исключения можно не перечислять
+        try(InputStream f1 = new FileInputStream("input.txt"); //очень хорошо что используешь try-with-resources
+            BufferedInputStream bis = new BufferedInputStream(f1)) {// bis не нужен, см. bis.close
+            byte b[] = new byte[f1.available()];// попробуй переписать используя Stream API
             f1.read(b);
-            bis.close();
-            f1.close();
+            bis.close();// close не нужен, try-with-resources сам все сделает
+            f1.close();// аналогично выше
             return new String(b, "utf8");
-        }
+        }// ловить исключения лучше в том методе где они могут выкидываться (исключением может быть общий ExceptionHandler для всего приложения, но он будет позже, когда доберемся до спринга)
     }
     
-    public static void writeFile(Hashtable<String,Integer> result) throws FileNotFoundException, IOException, Exception{
-        Date dateNow = new Date();
+    public static void writeFile(Hashtable<String,Integer> result) throws Exception{
+        Date dateNow = new Date();//советую присмотреться к "новому" API - LocalDateTime, в данном примере его использование не приципиально, но может он больше
         SimpleDateFormat formatForDateNow = new SimpleDateFormat("hh.mm.ss");
         String nameFile = "ouput_" + formatForDateNow.format(dateNow) +  ".txt";
         try (FileWriter f0 = new FileWriter(nameFile, true)) {
@@ -75,7 +77,7 @@ public class NeoFlexprojectTask2 {
                 String word = (String)itr.next();
                 f0.append("Количество встречающихся слов '" + word + "' "
                         + "в тексте равно " + (int) countWords.get(word) + "\n");
-            }
+            } // исключения лучше хэндлить в этом методе. FileNotFoundException выкидывается только на чтение, здесь его не будет
         }
     }
 }
