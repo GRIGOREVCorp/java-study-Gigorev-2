a4390676ac903ce56f6e1781d4c1898cb952d68f
Dima Grigorev <grigorev1997vip@inbox.ru>
2018-11-24 16:34:56 +0300

    ErrorCorrection2

--- src/neoflexprojecttask2/NeoFlexprojectTask2.java
+++ src/neoflexprojecttask2/NeoFlexprojectTask2.java
@@ -4,9 +4,16 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.*;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
+import javafx.util.converter.LocalDateTimeStringConverter;
 import org.apache.log4j.Logger;
+import sun.util.resources.LocaleData;
 
 public class NeoFlexprojectTask2 {
     private static final Logger LOGGER = Logger.getLogger(NeoFlexprojectTask2.class);
@@ -14,29 +21,11 @@ public class NeoFlexprojectTask2 {
     static Hashtable<String,Integer> countWords;
 
     public static void main(String[] args) {
-        try {        
             writeFile(searchWord("String private public", true));
-        } catch (FileNotFoundException ex1) { LOGGER.info(ex1); }
-          catch (IOException ex2) { LOGGER.warn(ex2); }
-          catch (Exception ex3) { LOGGER.error(ex3); }
     }
     
     private static Hashtable<String,Integer> searchWord(String word, boolean ignorCase) {
         countWords = new Hashtable<String,Integer>();
-        try {
-            /*String[] wordsFromTheText = transformationInMassiv(readFile());        
-            String[] wordsFromTheWord = transformationInMassiv(word);
-            int count;
-            for (String d : wordsFromTheWord) {
-                count=0;
-                for (String f : wordsFromTheText) {
-                    if (((d.compareToIgnoreCase(f))==0)&&(ignorCase==true))
-                        count++;
-                    else if((d.compareTo(f))==0) 
-                            count++;
-                }
-                countWords.put(d,count);
-            }*/
             List<String> text = readingFileInCollection();
             String[] wordsFromTheWord = transformationInMassiv(word);
             int count;
@@ -51,42 +40,34 @@ public class NeoFlexprojectTask2 {
                 countWords.put(d,count);
             }
             return countWords;
-        } catch (FileNotFoundException ex1) { LOGGER.info(ex1); }
-          catch (IOException ex2) { LOGGER.warn(ex2); }
-          catch (Exception ex3) { LOGGER.error(ex3); }
-        return null;
     }
     
     private static String[] transformationInMassiv(String text) {
         return text.replaceAll("[^A-Za-z]", " ").split(" ");
     }
     
-    /*private static String readFile() throws Exception{        
-        try(InputStream input = new FileInputStream("input.txt")) {
-            byte b[] = new byte[input.available()];
-            input.read(b);
-            return new String(b, "utf8");
-        }
-    }*/
-    
-    private static List<String> readingFileInCollection() throws IOException {
-        List<String> text = Files.lines(Paths.get("input.txt"))
-            .map(line -> line.replaceAll("[^A-Za-z]", " ").split(" ")).flatMap(Arrays::stream)
-            .collect(Collectors.toList());
-        return text;
+    private static List<String> readingFileInCollection() {
+        try {
+            List<String> text = Files.lines(Paths.get("input.txt"))
+                    .map(line -> line.replaceAll("[^A-Za-z]+", " ").split(" "))
+                    .flatMap(Arrays::stream)
+                    .collect(Collectors.toList());                    
+            return text;
+        } catch (FileNotFoundException notFound) { LOGGER.info(notFound); }
+          catch (IOException ex) { LOGGER.warn(ex); }
+          catch (Exception e) { LOGGER.error(e); }
+        return null;
     }
     
-    private static void writeFile(Hashtable<String,Integer> result) throws Exception{
-        Date dateNow = new Date();
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("hh.mm.ss");
-        String nameFile = "ouput_" + formatForDateNow.format(dateNow) +  ".txt";
+    private static void writeFile(Hashtable<String,Integer> result) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("hh.mm.ss");
+        String nameFile = "ouput_"+LocalTime.now().format(format)+".txt";
         try (FileWriter f0 = new FileWriter(nameFile, true)) {
-            Iterator itr = countWords.keySet().iterator();
-            while(itr.hasNext()) {
-                String word = (String)itr.next();
+            for (String word : countWords.keySet()) {
                 f0.append("Количество встречающихся слов '" + word + "' "
-                        + "в тексте равно " + (int) countWords.get(word) + "\n");
+                        + "в тексте равно " + countWords.get(word) + "\n");
             }
-        }
+        } catch (IOException ex) { LOGGER.warn(ex); }
+          catch (Exception e) { LOGGER.error(e); }
     }
 }
